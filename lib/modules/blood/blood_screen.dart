fda318e17c31c1071e06613f5a5f02fc7040d84f
Mohamed <ma422008@gmail.com>
2023-03-01 17:25:53 +0200

    Markers

--- lib/modules/blood/blood_screen.dart
+++ lib/modules/blood/blood_screen.dart
@@ -6,6 +6,8 @@ import '../../shared/cubit/cubit.dart';
 import '../../shared/cubit/states.dart';
 
 class BloodScreen extends StatelessWidget {
+  const BloodScreen({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,24 +37,228 @@ class BloodScreen extends StatelessWidget {
           ),
 
           body: GoogleMap(
-            initialCameraPosition: CameraPosition(
+            initialCameraPosition: const CameraPosition(
                 target: LatLng(30.033333, 31.233334),
                 zoom: 10
             ),
+
+
             onMapCreated: (GoogleMapController googleMapController) {
+              AppCubit.get(context).onMapCreated(googleMapController);
+
+
               AppCubit.get(context).addMarker(
-                markerId: '1',
-                markerPosition: LatLng(30.033333, 31.233334),
+              markerId: '1',
+              markerPosition: const LatLng(30.05160665975826, 31.21054654046658),
+              infoWindow: const InfoWindow(
+              title:'المركز القومى لنقل الدم',
+              snippet: 'العجوزة، حي العجوزة، الجيزة 3753530'),
+                icon: BitmapDescriptor.defaultMarker,
               );
+
+              AppCubit.get(context).addMarker(
+                  markerId: '2',
+                  markerPosition: const LatLng(30.070037976343123, 31.284155908532693),
+                  infoWindow: const InfoWindow(
+                      title: 'المركز الإقليمي لنقل الدم بالعباسية',
+                      snippet: ' امام, مدرسة '),
+                icon: BitmapDescriptor.defaultMarker,
+                );
+
+                AppCubit.get(context).addMarker(
+                  markerId: '3',
+                  markerPosition: const LatLng(30.015734113461725, 31.22772343015096),
+                  infoWindow: const InfoWindow(
+                    title: 'المركز الاقليمى لنقل الدم وتجميع البلازما بدار السلام',
+                    snippet: 'استكشاف المركز الاقليمى لنقل الدم وتجميع البلازما بدار السلام',
+                  ),
+                  icon: BitmapDescriptor.defaultMarker,
+                );
+                AppCubit.get(context).addMarker(
+                  markerId: '4',
+                  markerPosition: const LatLng(30.043390224368093, 31.217853117984525),
+                  infoWindow: const InfoWindow(
+                    title: 'مركز نقل الدم بمستشفى الدكتور مجدى',
+                    snippet: 'شارع بولس حنا، الدقي قسم، قسم الدقي، الجيزة 3753410',
+                  ),
+                  icon: BitmapDescriptor.defaultMarker,
+                );
+                AppCubit.get(context).addMarker(
+                  markerId: '5',
+                  markerPosition: const LatLng(
+                      30.065843713162607, 31.244354625377646),
+                  infoWindow: const InfoWindow(
+                    title: 'بنك الدم المركزى',
+                    snippet: ' الهلال الأحمر المصري، الجيارة، الأزبكية، محافظة القاهرة 4320151',
+                  ),
+                  icon: BitmapDescriptor.defaultMarker,
+
+                );
+                AppCubit.get(context).addMarker(
+                  markerId: '6',
+                  markerPosition: const LatLng(
+                      30.05192076644373, 31.211052163215275),
+                  infoWindow: const InfoWindow(
+                    title: 'المركز القومى لنقل الدم National Blood Transfusion Center, 26X6+MCG',
+                    snippet: 'العجوزة، العمرانية, الجيزة 3753530',
+                  ),
+                  icon: BitmapDescriptor.defaultMarker,
+                );
+
+                AppCubit.get(context).addMarker(
+                  markerId: '7',
+                  markerPosition: const LatLng(
+                      30.04484433519289, 31.210344506356613),
+                  infoWindow: const InfoWindow(
+                    title: 'مركز نقل الدم ومشتقاته بمستشفي الزراعيين',
+                    snippet: '26R6+W57، شارع وزارة الزراعة، الدقي، قسم الدقي، الجيزة 3751254',
+                  ),
+                  icon: BitmapDescriptor.defaultMarker,
+                );
+
+
+              AppCubit.get(context).addMarker(
+                markerId: '8',
+                markerPosition: const LatLng(
+                    30.037857242715162, 31.209218132614158),
+                infoWindow: const InfoWindow(
+                  title: 'المركز القومى لنقل الدم',
+                  snippet: ',26M6+P4 قسم الدقي,15 May Bridge، الدكتور السبكي',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+              AppCubit.get(context).addMarker(
+                markerId: '9',
+                markerPosition: const LatLng(
+                    30.05212242683355, 31.211021180236536),
+                infoWindow: const InfoWindow(
+                  title: 'بنك الدم السويسري، المستشفي السويسري',
+                  snippet: ',0237613117، العجوزة، حي العجوزة، الجيزة 3753530',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+              AppCubit.get(context).addMarker(
+                markerId: '10',
+                markerPosition: const LatLng(
+                    30.054054012579744, 31.210334534735367),
+                infoWindow: const InfoWindow(
+                  title: 'المركز القومى لنقل الدم',
+                  snippet: 'البطل أحمد عبد العزيز امام بنك hsbc,، العمرانية، الجيزة,0233374317',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+              AppCubit.get(context).addMarker(
+                markerId: '11',
+                markerPosition: const LatLng(
+                    30.045155859720996, 31.23022679471311),
+                infoWindow: const InfoWindow(
+                  title: 'المركز الاقليمى لنقل الدم',
+                  snippet: '987 كورنيش النيل، باب اللوق، عابدين، محافظة القاهرة 4272040',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+              AppCubit.get(context).addMarker(
+                markerId: '12',
+                markerPosition: const LatLng(
+                    30.07346400739051, 31.28485888329649),
+                infoWindow: const InfoWindow(
+                  title: 'بنك الدم الاقليمي',
+                  snippet: '378M+9X5، Unnamed Road, السرايات، الوايلى،، الوايلى، محافظة القاهرة',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+              AppCubit.get(context).addMarker(
+                markerId: '13',
+                markerPosition: const LatLng(
+                    30.02323041387819, 31.237252230235853),
+                infoWindow: const InfoWindow(
+                  title: 'بنك الدم 57357',
+                  snippet: ',0225351500,Children Canser Hospital, زينهم، قسم السيدة زينب، محافظة القاهرة 4260102',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+              AppCubit.get(context).addMarker(
+                markerId: '14',
+                markerPosition: const LatLng(
+                    30.073166901070287, 31.281462942806964),
+                infoWindow: const InfoWindow(
+                  title: 'Greek Hospital Blood Bank :: بنك الدم المستشفى اليوناني',
+                  snippet: '15 شارع السرايات, العباسية,0226836668',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+              AppCubit.get(context).addMarker(
+                markerId: '15',
+                markerPosition: const LatLng(
+                    30.025424652831912, 31.237583933650427),
+                infoWindow: const InfoWindow(
+                  title: 'مستشفى سرطان الاطفال 57357',
+                  snippet: ',0225351500,سكة حديد المحجر، زينهم، قسم السيدة زينب، محافظة القاهرة 4260102',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+
+
+              AppCubit.get(context).addMarker(
+                markerId: '16',
+                markerPosition: const LatLng(
+                    30.029817886746002, 31.232273542392065),
+                infoWindow: const InfoWindow(
+                  title: 'جمعية اصدقاء المبادرة القومية ضد السرطان',
+                  snippet: '33 القصر العيني، العيني، قسم السيدة زينب، محافظة القاهرة,4260016,0225353040,',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+              AppCubit.get(context).addMarker(
+                markerId: '17',
+                markerPosition: const LatLng(
+                    30.044517238506575, 31.210157081923427),
+                infoWindow: const InfoWindow(
+                  title: 'مستشفى الزراعيين',
+                  snippet: '1 ش النهضة بجوار وزارة الزراعة، شارع وزارة الزراعة، الدقي، قسم الدقي، الجيزة,0233377677',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+              AppCubit.get(context).addMarker(
+                markerId: '18',
+                markerPosition: const LatLng(
+                    30.605758347137343, 31.006484897177497),
+                infoWindow: const InfoWindow(
+                  title: 'المركز الاقليمى لنقل الدم بشبين الكوم',
+                  snippet: 'H2F6+VR2،,0482331379,قسم شبين الكوم، المنوفية 6132703',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+              AppCubit.get(context).addMarker(
+                markerId: '19',
+                markerPosition: const LatLng(
+                    30.025721902404115, 31.237927256401008),
+                infoWindow: const InfoWindow(
+                  title: 'مستشفى د. محمد الشبراويشى',
+                  snippet: '14 ميدان، السد العالي، فينى سابقا، قسم الدقي، الجيزة,0237606444',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              );
+
+
             },
+
             markers: AppCubit.get(context).myMarkers,
             // onMapCreated:AppCubit.get(context).onMapCreated,
           ),
         );
 
-
-
       },
     );
   }
 }
+
--- lib/shared/cubit/cubit.dart
+++ lib/shared/cubit/cubit.dart
@@ -16,8 +16,8 @@ class AppCubit extends Cubit<AppStates> {
 AppCubit() :super (AppInitialState());
 
 var myMarkers = HashSet<Marker>();
-int currentPage = 0;
-final pageController = PageController();
+
+
 final GlobalKey<AnimatedFloatingActionButtonState> key =GlobalKey<AnimatedFloatingActionButtonState>();
 
 int currentIndex=0;
@@ -49,16 +49,188 @@ static AppCubit get(context) =>BlocProvider.of(context);
 
 void addMarker(
   {
+     context,
     required String markerId,
     required  markerPosition,
-
+    required InfoWindow infoWindow,
+    required BitmapDescriptor icon,
   })
 {
+
   myMarkers.add(Marker(
-    markerId:MarkerId(markerId),
+    markerId:MarkerId('1'),
     position: markerPosition,
+    infoWindow:const InfoWindow(
+        title:'المركز القومى لنقل الدم,',
+        snippet: '0237613117,العجوزة، حي العجوزة، الجيزة 3753530'),
+    icon: BitmapDescriptor.defaultMarker,
+   // visible: true,
+  )
+  );
+
+  myMarkers.add(Marker(
+    markerId: MarkerId('2'),
+      position: markerPosition,
+    infoWindow: const InfoWindow(
+        title: 'المركز الإقليمي لنقل الدم بالعباسية',
+        snippet: ' امام, مدرسة,0226858645 '),
+    icon: BitmapDescriptor.defaultMarker,
+  ) );
+
+  myMarkers.add(Marker(
+    markerId: MarkerId('3'),
+    position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'المركز الاقليمى لنقل الدم وتجميع البلازما بدار السلام',
+      snippet: '987 كورنيش النيل، مصر القديمة،، محافظة القاهرة,0223619402',
+    ),
+  ) );
+  myMarkers.add(Marker(
+    markerId: MarkerId('4'),
+    position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'مركز نقل الدم بمستشفى الدكتور مجدى',
+      snippet: 'شارع بولس حنا، الدقي قسم، قسم الدقي، الجيزة ,0233350949,3753410',
+    ),
   ));
-  emit(AppAddMarkerState());
+  myMarkers.add(Marker(
+    markerId: MarkerId('5'),
+    position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'بنك الدم المركزى - الهلال الأحمر المصري',
+      snippet: 'الجيارة، الأزبكية، محافظة القاهرة ,0225761587,4320151',
+    ),
+
+  ));
+  myMarkers.add(Marker(
+    markerId: MarkerId('6'),
+    position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'المركز القومى لنقل الدم National Blood Transfusion Center, 26X6+MCG',
+      snippet: ',037613117,العجوزة، العمرانية, الجيزة 3753530',
+    ),
+  ) );
+
+      myMarkers.add(Marker(
+      markerId: MarkerId('7'),
+  position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'مركز نقل الدم ومشتقاته بمستشفي الزراعيين',
+      snippet: ',0237493226,26R6+W57، شارع وزارة الزراعة، الدقي، قسم الدقي، الجيزة 3751254',
+    ),
+));
+
+  ////////////////
+
+myMarkers.add(Marker(
+markerId: MarkerId('8'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'المركز القومى لنقل الدم',
+      snippet: ',26M6+P4 قسم الدقي,15 May Bridge، الدكتور السبكي',
+    ),
+));
+
+myMarkers.add(Marker(
+markerId: MarkerId('9'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'بنك الدم السويسري، المستشفي السويسري',
+      snippet: ',0237613117، العجوزة، حي العجوزة، الجيزة 3753530',
+    ),
+)  );
+
+myMarkers.add(Marker(
+markerId: MarkerId('10'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'المركز القومى لنقل الدم',
+      snippet: 'البطل أحمد عبد العزيز امام بنك hsbc,، العمرانية، الجيزة,0233374317',
+    ),
+) );
+
+myMarkers.add(Marker(
+markerId: MarkerId('11'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'المركز الاقليمى لنقل الدم',
+      snippet: '987 كورنيش النيل، باب اللوق، عابدين، محافظة القاهرة 4272040',
+    ),
+) );
+
+myMarkers.add(Marker(
+markerId: MarkerId('12'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'بنك الدم الاقليمي',
+      snippet: '378M+9X5، Unnamed Road, السرايات، الوايلى،، الوايلى، محافظة القاهرة',
+    ),
+) );
+myMarkers.add(Marker(
+markerId: MarkerId('13'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'بنك الدم 57357',
+      snippet: ',0225351500,Children Canser Hospital, زينهم، قسم السيدة زينب، محافظة القاهرة 4260102',
+    ),
+) );
+
+myMarkers.add(Marker(
+markerId: MarkerId('14'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'Greek Hospital Blood Bank :: بنك الدم المستشفى اليوناني',
+      snippet: '15 شارع السرايات, العباسية,0226836668',
+    ),
+) );
+
+myMarkers.add(Marker(
+markerId: MarkerId('15'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'مستشفى سرطان الاطفال 57357',
+      snippet: ',0225351500,سكة حديد المحجر، زينهم، قسم السيدة زينب، محافظة القاهرة 4260102',
+    ),
+)  );
+
+
+
+myMarkers.add(Marker(
+markerId: MarkerId('16'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'جمعية اصدقاء المبادرة القومية ضد السرطان',
+      snippet: '33 القصر العيني، العيني، قسم السيدة زينب، محافظة القاهرة,4260016,0225353040,',
+    ),
+) );
+
+myMarkers.add(Marker(
+markerId: MarkerId('17'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'مستشفى الزراعيين',
+      snippet: '1 ش النهضة بجوار وزارة الزراعة، شارع وزارة الزراعة، الدقي، قسم الدقي، الجيزة,0233377677',
+    ),
+) );
+
+myMarkers.add(Marker(
+markerId: MarkerId('18'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'المركز الاقليمى لنقل الدم بشبين الكوم',
+      snippet: 'H2F6+VR2،,0482331379,قسم شبين الكوم، المنوفية 6132703',
+    ),
+) );
+myMarkers.add(Marker(
+markerId: MarkerId('19'),
+position: markerPosition,
+    infoWindow: const InfoWindow(
+      title: 'مستشفى د. محمد الشبراويشى',
+      snippet: '14 ميدان، السد العالي، فينى سابقا، قسم الدقي، الجيزة,0237606444',
+    ),
+)  );
+
+emit(AppAddMarkerState());
 }
 
   void changeIndex(int index) {
