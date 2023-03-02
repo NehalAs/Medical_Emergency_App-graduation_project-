c9f9b0cac0a4a63d474a50c2d56889cdb572a813
Mohamed <ma422008@gmail.com>
2023-03-03 00:45:35 +0200

    All Blood Markers Done

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -153,7 +153,480 @@ class HomeScreen extends StatelessWidget {
             infoWindowTitle: 'مستشفى د. محمد الشبراويشى',
             infoWindowDescription: '14 ميدان، السد العالي، فينى سابقا، قسم الدقي، الجيزة,0237606444',
           );
-        },
+
+
+          AppCubit.get(context).addMarker(
+            markerId: '20',
+            markerPosition: const LatLng(
+                30.06389801557521, 31.259661948463698 ),
+
+            infoWindowTitle: 'مستشفى باب الشعرية (سيد جلال)',
+            infoWindowDescription: ',0225893754,3766+222، Al Shareaa Al Gadid، العدوي، باب الشعرية, العدوي، محافظة القاهرة 4330360',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          AppCubit.get(context).addMarker(
+            markerId: '21',
+            markerPosition: const LatLng(
+                30.06833436735362, 31.29338304819735),
+
+            infoWindowTitle: 'مستشفى الامراض الصدرية بالعباسية',
+            infoWindowDescription: 'العباسية، 6 السكة البيضاء، ABBASSEYA، مدينة نصر، محافظة القاهرة,0223425245,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '22',
+            markerPosition: const LatLng(
+                30.602608831652418, 31.482351105606053 ),
+
+            infoWindowTitle: 'بنك الدم مستشفى جامعه الزقازيق',
+            infoWindowDescription: 'HFMP+JW9، شيبة النكارية، مركز الزقازيق، الشرقية 7120001',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '23',
+            markerPosition: const LatLng(
+                30.030469867303026, 31.23626741984464),
+
+            infoWindowTitle: 'مستشفى أطفال مصر',
+            infoWindowDescription: ',01004244428,1 Abu el reesh، ميدان، السيدة زينب، محافظة القاهرة‬',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '24',
+            markerPosition: const LatLng(
+                30.069819962462216, 31.294815243074865),
+
+            infoWindowTitle: 'مستشفى حميات العباسية',
+            infoWindowDescription: ',0223424025,El-Abaseya، إمتداد رمسيس، السريات الشرقية،، مدينة نصر',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '25',
+            markerPosition: const LatLng(
+                30.02058282917547, 32.54624379111682),
+
+            infoWindowTitle: 'المركز الاقليمي لنقل الدم بالسويس',
+            infoWindowDescription: 'XG8W+XX2، الشهداء، السويس،، السويس',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '26',
+            markerPosition: const LatLng(
+                29.988970802929213, 31.272705680432043),
+
+            infoWindowTitle: 'مستشفى الياسمين بالمعادي - Yasmeen Hospital Al Maadi',
+            infoWindowDescription: ',01014111130,X7MF+J37، الجزائر، معادي السرايات الغربية، قسم البساتين، محافظة القاهرة 4234501',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          AppCubit.get(context).addMarker(
+            markerId: '27',
+            markerPosition: const LatLng(
+                30.0346310683986, 31.2268260439908),
+
+            infoWindowTitle: 'مستشفيات جامعة القاهرة',
+            infoWindowDescription: ',0223643988,26JG+JRF، قسم مصر القديمة، محافظة القاهرة 4240310',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '28',
+            markerPosition: const LatLng(
+                30.05152250319907, 31.26607451475684),
+
+            infoWindowTitle: 'مستشفى الحسين التخصصي',
+            infoWindowDescription: 'جوهر القائد، Ad، الدرب الأحمر، محافظة القاهرة ,01020186351,4293076',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '29',
+            markerPosition: const LatLng(
+                30.084496646151642, 31.28966540160409),
+
+            infoWindowTitle: 'مستشفى عين شمس التخصصي',
+            infoWindowDescription: 'الخليفة المأمون، كوبري القبة، قسم مصر الجديدة، محافظة القاهرة ,01098106892,4393002',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '30',
+            markerPosition: const LatLng(
+                31.286739379431644, 29.91112654199217 ),
+
+            infoWindowTitle: 'المركز الاقليمى لنقل الدم بالاسكندرية',
+            infoWindowDescription: '63 أحمد سليمان الشيخ، كوم الدكة شرق، العطارين، الإسكندرية ,034951963,5370055',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '31',
+            markerPosition: const LatLng(
+                31.278235682989365, 29.91661969759209 ),
+
+            infoWindowTitle: 'بنك الدم',
+            infoWindowDescription: 'مستشفي الجامعة، باب شرقي ووابور المياه، قسم باب شرقي، الإسكندرية ,01281813497,5422023',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          AppCubit.get(context).addMarker(
+            markerId: '33',
+            markerPosition: const LatLng(
+                31.32799950109503, 29.955071683502787),
+
+            infoWindowTitle: 'بنك الدم بالشاطبي',
+            infoWindowDescription: ',034871586,6XP2+M7H، طريق الجيش، الأزاريتة والشاطبي، قسم باب شرقي، الإسكندرية 5452041',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '34',
+            markerPosition: const LatLng(
+                31.075047782730014, 31.363274801800014),
+
+            infoWindowTitle: 'بنك دم _ مستشفى الجامعة',
+            infoWindowDescription: '29V7+5MM، اول المنصورة،، اول المنصورة، الدقهلية',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '35',
+            markerPosition: const LatLng(
+                30.027932405096088, 31.23094591709254),
+
+            infoWindowTitle: 'المعهد القومي للأورام',
+            infoWindowDescription: '1 شارع القصر العيني، ميدان فم الخليج، العيني، قسم السيدة زينب، محافظة القاهرة,0223664690,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '36',
+            markerPosition: const LatLng(
+                30.144562212053618, 31.355737632984656 ),
+
+            infoWindowTitle: 'مستشفى عين شمس العام',
+            infoWindowDescription: '49J4+V7R، عين شمس الشرقية، قسم عين شمس، محافظة القاهرة 4545245',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '37',
+            markerPosition: const LatLng(
+                30.03329355852859, 31.23094591709066 ),
+
+            infoWindowTitle: 'مستشفى قصر العيني التعليمي الجديد',
+            infoWindowDescription: 'كورنيش النيل، العيني، قسم السيدة زينب، محافظة القاهرة ,0223654061,4262001',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          AppCubit.get(context).addMarker(
+            markerId: '39',
+            markerPosition: const LatLng(
+                30.640302483584918, 32.28745098043834),
+
+            infoWindowTitle: 'المركز الإقليمى لخدمات نقل الدم بالإسماعيلية',
+            infoWindowDescription: 'البحيرة، قسم ثان الاسماعيلية، الإسماعيلية 41517,0643103835,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '40',
+            markerPosition: const LatLng(
+                30.147730114465325, 31.385083586592234 ),
+
+            infoWindowTitle: 'Saudi German Hospital Cairo المستشفي السعودي الألماني القاهرة',
+            infoWindowDescription: 'محور طه حسين، الهايكستب، قسم النزهة، محافظة القاهرة 4473303',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          ////
+
+          AppCubit.get(context).addMarker(
+            markerId: '41',
+            markerPosition: const LatLng(
+                30.033671660663572, 31.227995197038087 ),
+
+            infoWindowTitle: 'مستشفى قصر العيني',
+            infoWindowDescription: 'جزيرة,0223643524,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '42',
+            markerPosition: const LatLng(
+                29.081860672402183, 31.121403769913048),
+
+            infoWindowTitle: 'بنك الدم الاقليمى بنى سويف',
+            infoWindowDescription: '24R9+Q8M، قسم بني سويف، مركز بنى سويف، محافظة بني سويف 2731016',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '43',
+            markerPosition: const LatLng(
+                29.089061350144544, 31.12415035175445),
+
+            infoWindowTitle: 'المركز القليمى لنقل الدم ببنى سويف',
+            infoWindowDescription: ',0822245151,24R9+R97، قسم بني سويف، بياض العرب، مركز بنى سويف، محافظة بني سويف 2731016',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '44',
+            markerPosition: const LatLng(
+                30.098493746618168, 31.328491733014516),
+
+            infoWindowTitle: 'مستشفى كليوباترا',
+            infoWindowDescription: ',0224143931,39 Cleopatra Street, Opposite Maidan Salahuddin Square، الماظة، قسم مصر الجديدة',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '45',
+            markerPosition: const LatLng(
+                31.28518249059463, 29.894647072060863),
+
+            infoWindowTitle: 'مركز التبرع بالبلازما الأسكندرية',
+            infoWindowDescription: ',01551978533,5WW5+9MQ، الإسكندرية،، غرب، العطارين،، الإسكندرية',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '46',
+            markerPosition: const LatLng(
+                30.09881289972861, 31.291096016095015),
+
+            infoWindowTitle: 'مستشفى الأمل التخصصي',
+            infoWindowDescription: 'عرفات، الوايلي الكبير شرق، قسم حدائق القبة، محافظة القاهرة ,0224531119,4382542',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '47',
+            markerPosition: const LatLng(
+                30.558768964685054, 31.24876563277087 ),
+
+            infoWindowTitle: 'المركز الإقليمى لنقل الدم ببنها فرع أسنيت',
+            infoWindowDescription: 'G6HX+PG3، أسنيت، مركز كفر شكر، القليوبية 6491644',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          AppCubit.get(context).addMarker(
+            markerId: '48',
+            markerPosition: const LatLng(
+                30.235665930534893, 31.481944237700983),
+
+            infoWindowTitle: 'مستشفى فريد حبيب',
+            infoWindowDescription: 'مستشفى فريد حبيب الحى الخامس قطعة رقم 5 بلوك 16081, قطعة رقم 5 Rock Ville Rd, العبور، القليوبية,0246142000,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '49',
+            markerPosition: const LatLng(
+                30.013448219960967, 32.54075066026421 ),
+
+            infoWindowTitle: 'المركز الاقليمي لنقل الدم بالسويس',
+            infoWindowDescription: 'XG8W+XX2، الشهداء، السويس،، السويس',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '50',
+            markerPosition: const LatLng(
+                28.175775838893017, 30.77265313647568 ),
+
+            infoWindowTitle: 'المركز الإقليمى لنقل الدم بالمنيا',
+            infoWindowDescription: 'كورنيش النيل بجوار مركز أورام المنيا Al menya, المنيا,0862339241,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '51',
+            markerPosition: const LatLng(
+                31.098290159302742, 31.368768004540723 ),
+
+            infoWindowTitle: 'مركز الأورام جامعة المنصورة',
+            infoWindowDescription: 'جيهان السادات، اول المنصورة، الدقهلية 7650030,0502202945,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '52',
+            markerPosition: const LatLng(
+                29.97783114756183, 32.550467211782376 ),
+
+            infoWindowTitle: 'مستشفى السويس العام',
+            infoWindowDescription: 'Salah Uddin St، السويس، 8141210,0623331190,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '53',
+            markerPosition: const LatLng(
+                30.072568428449916, 31.276650851960085 ),
+
+            infoWindowTitle: 'بنك الدم الرئيسي - عين شمس',
+            infoWindowDescription: '379G+64V، ممر خاص مستشفى الدمرداش، العباسية القبلية، الوايلى، محافظة القاهرة 4390220',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '54',
+            markerPosition: const LatLng(
+                30.589537635318045, 31.00670549709788 ),
+
+            infoWindowTitle: 'مستشفى شبين الكوم التعليمي',
+            infoWindowDescription: 'Gamal Abdel Nasser St، قسم شبين الكوم، المنوفية 6132703,0482221277,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '55',
+            markerPosition: const LatLng(
+                25.392690598608382, 46.63273463437496),
+
+            infoWindowTitle: 'بنك الدم بمستشفى مدينة الملك سعود الطبية',
+            infoWindowDescription: 'JMHQ+5WF، شارع الإمام عبدالعزيز بن محمد بن سعود الفرعي، عليشة، shemisee, الرياض 12746، المملكة العربية السعودية',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '56',
+            markerPosition: const LatLng(
+                30.819614617122227, 30.998463967644348),
+
+            infoWindowTitle: 'مستشفى المنشاوى العام',
+            infoWindowDescription: 'شارع مستشفي المنشاوي، طنطا (قسم 2)، طنطا،، الغربية',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          AppCubit.get(context).addMarker(
+            markerId: '58',
+            markerPosition: const LatLng(
+                26.78497064798173, 31.787543788537988),
+
+            infoWindowTitle: 'بنك الدم الاقليمي بسوهاج',
+            infoWindowDescription: 'GPQ4+99P، الخولي، قسم أول سوهاج، سوهاج 1670032,0932155381,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          AppCubit.get(context).addMarker(
+            markerId: '59',
+            markerPosition: const LatLng(
+                25.92936795138513, 32.852973613703305),
+
+            infoWindowTitle: 'مستشفى شفاء الأورمان Shefaa Al Orman Hospital',
+            infoWindowDescription: 'طيبة، مدينة طيبة الجديدة بجوار جهاز مدينة طيبة',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '60',
+            markerPosition: const LatLng(
+                30.99524147009114, 31.71881841207787),
+
+            infoWindowTitle: 'مستشفى أولاد صقر المركزي',
+            infoWindowDescription: 'قرية الزنانيرى، أولاد صقر، مركز أولاد صقر، الشرقية 7362860,0553464636,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '61',
+            markerPosition: const LatLng(
+                30.31647014595019, 31.743196960386587 ),
+
+            infoWindowTitle: 'مصر سكوب لتجهيز سيارات الاسعاف',
+            infoWindowDescription: 'قطعة 200 جنوب غرب 6 ا المنطقة الصناعية الثالثة, الشرقية ,01020010679,44634',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '62',
+            markerPosition: const LatLng(
+                30.585341380706012, 31.49883788802093 ),
+
+            infoWindowTitle: 'المركز الاقليمى لنقل الدم بالزقازيق',
+            infoWindowDescription: 'HG82+JGR، طريق الأحرار، مركز الزقازيق، الشرقية 7141360',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '63',
+            markerPosition: const LatLng(
+                27.33111817013578, 31.206331644086358 ),
+
+            infoWindowTitle: 'Assiut Regional Blood Transfusion Center المركز الإقليمي لنقل الدم فرع أسيوط',
+            infoWindowDescription: ',0882370016,55HJ+QPC، الطريق الدائري، الحمراء الثانية، قسم ثان أسيوط، أسيوط 2073021',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '64',
+            markerPosition: const LatLng(
+                30.03383715734181, 31.43728258160833  ),
+
+            infoWindowTitle: 'Health Care City',
+            infoWindowDescription: '90 التسعين الشمالي، قسم أول القاهرة الجديدة، محافظة القاهرة ,0228124242,4730131',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '65',
+            markerPosition: const LatLng(
+                30.074351063771537, 31.275756983685152),
+
+            infoWindowTitle: 'بنك الدم الرئيسي - عين شمس',
+            infoWindowDescription: '379G+64V، ممر خاص مستشفى الدمرداش، العباسية القبلية، الوايلى، محافظة القاهرة 4390220',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '66',
+            markerPosition: const LatLng(
+                30.04357842265291, 31.21672725544704 ),
+
+            infoWindowTitle: 'مستشفى د. محمد الشبراويشى',
+            infoWindowDescription: '14 ميدان، السد العالي، فينى سابقا، قسم الدقي، الجيزة,0237606444,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '67',
+            markerPosition: const LatLng(
+                31.24454271564153, 29.920350270551847),
+
+            infoWindowTitle: 'بنك الدم فرع كوم الدكة',
+            infoWindowDescription: 'كوم الدكة غرب، العطارين، الإسكندرية 5370055,034951963,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+          AppCubit.get(context).addMarker(
+            markerId: '68',
+            markerPosition: const LatLng(
+                31.090402009976852, 31.382500878474165 ),
+
+            infoWindowTitle: 'بنك الدم المنصورة الشيخ حسنين',
+            infoWindowDescription: '29RH+CFQ، رقم 1، المنصورة (قسم 2)، المنصورة، الدقهلية 7652110,0502396781,',
+
+            icon: BitmapDescriptor.defaultMarker,
+          );
+
+          },
           markers: AppCubit.get(context).myMarkers,
         );
       },
